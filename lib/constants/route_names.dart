9c86ac0c165b54516e59bbfedb1ca4c24b72a603
Salayna <salaynadoukoure@gmail.com>
2021-04-26 12:21:58 +0200

    WIP travel commit

---
+++ lib/constants/route_names.dart
@@ -0,0 +1,5 @@
+class RouteNames {
+  static const String home = '/';
+  static const String login = '/signin';
+  static const String signup = '/signup';
+}
---
+++ lib/ui/search_and_discovery_view.dart/search_and_discovery_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchAndDiscoveryView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Third Page"),
+    );
+  }
+}
